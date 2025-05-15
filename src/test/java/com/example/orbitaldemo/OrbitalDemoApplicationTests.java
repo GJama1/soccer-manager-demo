c3a9bb059a796e40e1997acc42720c1443e7f8f9
GJama1 <gjama730@gmail.com>
2025-05-15 15:27:47 +0400

    Add test config

--- src/test/java/com/example/orbitaldemo/OrbitalDemoApplicationTests.java
+++ src/test/java/com/example/orbitaldemo/OrbitalDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.orbitaldemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrbitalDemoApplicationTests {
 
     @Test
